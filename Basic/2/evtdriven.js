88a15d7c004839bf084f8f68e94e2a3b114fdd0a
loosie <jong9712@naver.com>
2020-06-17 18:21:03 +0900

    pratice Express(로그인)

--- Basic/2/evtdriven.js
+++ Basic/2/evtdriven.js
@@ -17,12 +17,15 @@ function t1(){
   console.log('ㄱㄷ');
   setTimeout( ()=>{
     console.log('t1');
-  },0);
+  },10);
   console.log('ㄱㄷ2');
 }
 
 function t2() {
   console.log('t2');
+  setTimeout( ()=>{
+    console.log('t5');
+  },10);
 }
 
 function t3() {
